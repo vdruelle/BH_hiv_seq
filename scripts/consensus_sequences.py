641ccfd5dbdca0b9b73bf41f428d7f1c1a5ccd60
Valentin <valentin.druelle@unibas.ch>
2021-04-20 16:55:53 +0200

    consensus sequence computation

--- scripts/consensus_sequences.py
+++ scripts/consensus_sequences.py
@@ -2,23 +2,31 @@ import sys
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-from Bio import AlignIO
+from Bio import AlignIO, SeqIO
 from Bio.Seq import Seq
 from Bio.Align import MultipleSeqAlignment
+from Bio.SeqRecord import SeqRecord
 
 alignment_file = sys.argv[1]
+output_file = sys.argv[2]
 
 alignment = AlignIO.read(alignment_file, "fasta")
 alignment_array = np.array(alignment)
 
-distance_matrix = (alignment_array != alignment_array[0,:])
-distance = np.sum(distance_matrix, axis=1, dtype=int) / distance_matrix.shape[-1]
+# Consensus computation
+consensus_sequence = []
+for ii in range(alignment_array.shape[-1]):
+    values, counts = np.unique(alignment_array[:,ii], return_counts=True)
+    consensus_nucleotide = values[np.argmax(counts)]
+    consensus_sequence = consensus_sequence + [consensus_nucleotide]
 
+consensus_sequence = np.array(consensus_sequence)
+consensus_sequence = Seq("".join(consensus_sequence))
+consensus_sequence = SeqRecord(seq=consensus_sequence, id=f"Consensus_{alignment_file}", name="", description="")
 
-hist, bins = np.histogram(distance, bins=20)
-bins = 0.5*(bins[1:] + bins[:-1])
+with open(output_file, "w") as handle:
+    SeqIO.write([consensus_sequence], handle, "fasta")
 
-plt.figure()
-plt.plot(bins, hist, '.-')
-plt.grid()
-plt.show()
+# Distance to consensus sequence
+# distance_matrix = (alignment_array != consensus_sequence)
+# distance = np.sum(distance_matrix, axis=1, dtype=int) / distance_matrix.shape[-1]
